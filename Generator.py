18ece617dfa28b1165f84e8ed86cbc769294f7d9
Alexander Chernenko <letmeexist95@gmail.com>
2025-03-07 23:57:59 +0200

    Implemented python file generator

--- Generator.py
+++ Generator.py
@@ -2,16 +2,16 @@ import random
 import struct
 
 
-def generate_1gb_txt(filename="output.txt", file_size_gb=1):
-    target_size = file_size_gb * 1024 * 1024 * 1024  # Convert GB to bytes
-    with open(filename, "w") as f:
+def generate_1gb_txt(filename='output.txt', file_size_gb=1):
+    target_size = file_size_gb * 1024 * 1024 * 1024 
+    with open(filename, 'w') as f:
         current_size = 0
         while current_size < target_size:
-            value = random.uniform(-1e6, 1e6)  # Generate a random double
-            line = f"{value:.15f}\n"  # Format to 15 decimal places
+            value = random.uniform(-1e6, 1e6)
+            line = f'{value:.15f}\n'
             f.write(line)
             current_size += len(line.encode("utf-8"))
 
 
 if __name__ == "__main__":
-    generate_1gb_txt("file.txt", 0.1)
+    generate_1gb_txt("big_file.txt", 1)
---
+++ Test/FileSorted.py
@@ -0,0 +1,45 @@
+import argparse
+
+def is_file_sorted(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            first_line = file.readline().strip()
+            
+            try:
+                prev_value = float(first_line)  # Convert first line to float
+            except ValueError:
+                print(f'[Error] "{first_line}" is not a valid number')
+                return False
+
+            for line in file:
+                line = line.strip()
+                
+                try:
+                    current_value = float(line)
+                except ValueError:
+                    print(f'[Error] "{line}" is not a valid number')
+                    return False
+
+                if prev_value > current_value:
+                    return False
+
+                prev_value = current_value
+
+        return True
+
+    except Exception as e:
+        print(f'[Error] {e}')
+
+        return False  
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    
+    args = parser.parse_args()
+    
+    if is_file_sorted(args.file):
+        print('[Success] The file is sorted')
+    else:
+        print('[Failure] The file is NOT sorted')
